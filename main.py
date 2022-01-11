5c00406adf8bc2acc0b02b0b8f2bd6123ed84108
kovalevevg <76981561+kovalevevg@users.noreply.github.com>
2022-01-11 20:01:57 +0300

    Task2
    Python code using the cryptography library 36.0.1

---
+++ main.py
@@ -0,0 +1,18 @@
+from cryptography.fernet import Fernet
+
+key = Fernet.generate_key()
+print(key)
+cipher = Fernet(key)
+
+with open('my_key.txt', 'wb') as f:
+    f.write(key)
+
+text_to_secure = open('secret_text.txt', 'rb').read()
+
+with open('my_secured_text.txt', 'wb') as f:
+    f.write(cipher.encrypt(text_to_secure))
+
+text_to_unsecure = open('my_secured_text.txt', 'rb').read()
+
+with open('my_unsecured_text.txt', 'wb') as f:
+    f.write(cipher.decrypt(text_to_unsecure))
